5f94cc6b028c01f5b5fd8c91188986968b74912b
sushmitha-reddy03 <reddysushu15@gmail.com>
2026-04-05 23:08:00 +0530

    Delete target directory

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
--- LifeDrop Blood Donor System - Schema Initialization
-
--- Use the database (Spring Boot will handle this if createDatabaseIfNotExist=true)
--- CREATE DATABASE IF NOT EXISTS blood_donor_db;
--- USE blood_donor_db;
-
--- The tables will be automatically created by Hibernate (spring.jpa.hibernate.ddl-auto=update)
--- But we can provide explicit schema here if needed. 
--- For now, let's just use data.sql for initial data.
